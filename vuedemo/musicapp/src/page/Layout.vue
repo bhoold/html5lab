0c86c274840bafc1f9c77d0c34a65adf83dd4f0a
Renven <bhoold@qq.com>
2017-06-18 15:57:42 +0800

    添加主菜单，播放器添加title提示

--- vuedemo/musicapp/src/page/Layout.vue
+++ vuedemo/musicapp/src/page/Layout.vue
@@ -2,7 +2,33 @@
   <div id="app">
     <div id="main">
       <div id="menu">
-
+        <div class="panel">
+          <div class="panel-heading top"><a class="iconfont icon-sort"></a></div>
+          <div class="panel-body">
+            <a class="item"><i class="iconfont icon-search"></i><span>搜索</span></a>
+            <a class="item"><i class="iconfont icon-searchlist"></i><span>发现音乐</span></a>
+            <a class="item"><i class="iconfont icon-radiobox"></i><span>MV</span></a>
+          </div>
+        </div>
+        <div class="panel">
+          <div class="panel-heading">我的音乐</div>
+          <div class="panel-body">
+            <a class="item"><i class="iconfont icon-home"></i><span>本地音乐</span></a>
+            <a class="item"><i class="iconfont icon-down"></i><span>下载管理</span></a>
+          </div>
+        </div>
+        <div class="panel">
+          <div class="panel-heading">创建的歌单</div>
+          <div class="panel-body">
+            <a class="item"><i class="iconfont icon-like"></i><span>我喜欢的音乐</span></a>
+          </div>
+        </div>
+        <div class="personal">
+          <img src="../images/avatar.jpg" />
+          <span>未登录</span>
+          <a><i class="iconfont icon-comment"></i></a>
+          <a><i class="iconfont icon-settings"></i></a>
+        </div>
       </div>
       <div id="content"></div>
     </div>
@@ -12,17 +38,17 @@
         <a class="mask">歌词</a>
       </div>
       <div class="operate">
-        <a><i class="iconfont icon-like"></i></a>
-        <a><i class="iconfont icon-all"></i></a>
-        <a><i class="iconfont icon-notification"></i></a>
-        <a><i class="iconfont icon-list"></i></a>
+        <a title="收藏"><i class="iconfont icon-like"></i></a>
+        <a title="均衡器"><i class="iconfont icon-all"></i></a>
+        <a title="音量"><i class="iconfont icon-notification"></i></a>
+        <a title="播放列表"><i class="iconfont icon-list"></i></a>
 
       </div>
       <div class="control">
         <div class="icons-btn">
-        <a class="prev"><i class="iconfont icon-fold"></i></a>
-        <a class="play"><i class="iconfont icon-stop"></i></a>
-        <a class="next"><i class="iconfont icon-unfold"></i></a>
+        <a class="prev" title="上一首"><i class="iconfont icon-fold"></i></a>
+        <a class="play" title="正在播放"><i class="iconfont icon-stop"></i></a>
+        <a class="next" title="下一首"><i class="iconfont icon-unfold"></i></a>
         </div>
         <div class="bar">
           <div class="label">水手</div>
@@ -30,7 +56,7 @@
             <span>00:00</span>
             <span>00:00</span>
           </div>
-          <div class="progress">
+          <div class="progress" title="播放进度">
             <div class="progress-bar">
               <a></a>
             </div>
@@ -62,6 +88,9 @@ export default {
 </script>
 
 <style lang="scss">
+*{
+    box-sizing: border-box;
+}
 html,body,#app{
   height: 100%;
 }
@@ -78,7 +107,103 @@ body {
   position: relative;
   background: #f3f3f5;
   width: 200px;
-  border: 1px solid #d3d3d8;
+  border-right: 1px solid #d3d3d8;
+      .panel{
+        margin-bottom: 15px;
+        border: none;
+        background: none;
+        -webkit-box-shadow: none;
+        box-shadow: none;
+        .panel-heading{
+        padding: 9px 12px;
+        border: none;
+        font-size: 12px;
+        color: #888;
+        }
+        .panel-body{
+          padding: 0;
+        }
+      }
+      .item{
+        display: block;
+        position: relative;
+        padding: 9px 12px;
+        &:hover{
+          background: #e8e8eb;
+        }
+        &.active{
+          background: #dddde1;
+          &:before{
+            content: '';
+            position: absolute;
+            top: 0;
+            left: -1px;
+            height: 100%;
+            width: 2px;
+            background: #32aaff;
+          }
+        }
+        span{
+          margin-left: 1em;
+        }
+      }
+      .personal{
+        position: absolute;
+        bottom: -2px;
+        left: 0;
+        padding: 14px 12px;
+        width: 100%;
+        height: 60px;
+        border-top: 1px solid #d3d3d8;
+        font-size: 12px;
+        background: #f3f3f5;
+        img{
+          width: 30px;
+          height: 30px;
+          margin-right: 10px;
+          border-radius: 50%;
+        }
+        a{
+          float: right;
+          margin-top: 4px;
+          margin-left: 20px;
+        }
+      }
+      &.simple{
+        width: auto;
+        .panel{
+          margin-bottom: 0;
+          border-bottom: 1px solid #d3d3d8;
+        .panel-heading{
+            display: none;
+            &.top{
+              display: block;
+            }
+          }
+        }
+        .item span{
+          display: none;
+        }
+        .personal{
+          padding: 0;
+          height: auto;
+          border-top: none;
+          text-align: center;
+
+          img{
+            margin: 9px 0;
+          }
+          span{
+            display: none;
+          }
+          a{
+            float: none;
+            display: block;
+            margin: 0;
+            padding: 9px 12px;
+          }
+        }
+      }
 }
 #content{
   flex: 1;
@@ -124,7 +249,6 @@ body {
       padding: 20px 27px 0;
       width: 250px;
       height: 100%;
-      box-sizing: border-box;
         a{
             margin-right: 30px;
         }
