142b814ae7b5ad72f1aefb7fd8e28f1d19a368ac
bhoold <bhoold@yahoo.com>
2018-07-09 11:19:51 +0800

    添加身份证验证js代码

---
+++ snippet/idCard.js
@@ -0,0 +1,15 @@
+//身份证验证，来自http://www.rzzx.com.cn/webs/UserViews/UserManager.aspx
+
+function isCardID(sId){ 
+	var iSum=0 ;
+	var info="" ;
+	if(!/^\d{17}(\d|x)$/i.test(sId)) return "你输入的身份证长度或格式错误"; 
+	sId=sId.replace(/x$/i,"a"); 
+	if(aCity[parseInt(sId.substr(0,2))]==null) return "你的身份证地区非法"; 
+	sBirthday=sId.substr(6,4)+"-"+Number(sId.substr(10,2))+"-"+Number(sId.substr(12,2)); 
+	var d=new Date(sBirthday.replace(/-/g,"/")) ;
+	if(sBirthday!=(d.getFullYear()+"-"+ (d.getMonth()+1) + "-" + d.getDate()))return "身份证上的出生日期非法"; 
+	for(var i = 17;i>=0;i --) iSum += (Math.pow(2,i) % 11) * parseInt(sId.charAt(17 - i),11) ;
+	if(iSum%11!=1) return "你输入的身份证号非法"; 
+	return true;//aCity[parseInt(sId.substr(0,2))]+","+sBirthday+","+(sId.substr(16,1)%2?"男":"女") 
+} 
